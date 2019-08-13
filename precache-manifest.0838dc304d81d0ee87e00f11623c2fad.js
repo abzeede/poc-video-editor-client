9d11067bf151ae08be5c5ae3df675d97ea21c782
bank <>
2019-08-13 14:16:51 +0700

    Updates

---
+++ precache-manifest.0838dc304d81d0ee87e00f11623c2fad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e07e9d5dce3917907daf66428231bca",
+    "url": "/poc-video-editor-client/index.html"
+  },
+  {
+    "revision": "85b6b0a0df5df7f66b3b",
+    "url": "/poc-video-editor-client/static/css/main.35a4a077.chunk.css"
+  },
+  {
+    "revision": "8f55aed3fd91d14b68d3",
+    "url": "/poc-video-editor-client/static/js/2.2a232ffc.chunk.js"
+  },
+  {
+    "revision": "85b6b0a0df5df7f66b3b",
+    "url": "/poc-video-editor-client/static/js/main.89bee645.chunk.js"
+  },
+  {
+    "revision": "4750ab4c6b3716eaf316",
+    "url": "/poc-video-editor-client/static/js/runtime~main.1a0b9709.js"
+  }
+]);
