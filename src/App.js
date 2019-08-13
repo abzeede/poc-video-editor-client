dc940e000fc275960bfb6e703efbbfb102a184f2
bank <>
2019-08-13 14:08:18 +0700

    remove unused package

--- src/App.js
+++ src/App.js
@@ -1,86 +1,71 @@
-import React, { useState } from 'react'
-import VideoEditor from './components/VideoEditor'
-import './app.css'
+import React, { useState } from "react";
+import VideoEditor from "./components/VideoEditor";
+import "./app.css";
 
-function extractFrames(file, startAt, endAt) {
+function trimVideo({ file, startAt, endAt, onProcessing }) {
   return new Promise((resolve, reject) => {
-    let video = document.createElement('video')
-    let images = []
-    let canvas = document.createElement('canvas')
-    let ctx = canvas.getContext('2d')
-    let pro = document.querySelector('#progress')
+    let video = document.createElement("video");
+    let images = [];
+    let canvas = document.createElement("canvas");
+    let preview = document.getElementById("preview-video");
+    let ctx = canvas.getContext("2d");
+    const mediaStream = canvas.captureStream(25);
+    preview.srcObject = mediaStream;
 
     function initCanvas(e) {
-      canvas.width = this.videoWidth
-      canvas.height = this.videoHeight
+      canvas.width = this.videoWidth;
+      canvas.height = this.videoHeight;
     }
+
+    function onEnd(e) {}
 
     function saveFrame(blob) {
       images.push(blob);
     }
 
-    function onEnd(e) {
-      resolve(images.map((image) => URL.createObjectURL(image)))
-    }
-
     function drawFrame(e) {
-      this.pause()
-      ctx.drawImage(video, 0, 0)
-      canvas.toBlob(saveFrame, 'image/jpeg')
-      pro.innerHTML = ((this.currentTime / endAt) * 100).toFixed(2) + ' %'
+      this.pause();
+      ctx.drawImage(video, 0, 0);
+      onProcessing(((this.currentTime / endAt) * 100).toFixed(2));
       if (this.currentTime < endAt) {
-        this.play()
+        this.play();
       } else {
-        onEnd()
+        onEnd();
       }
     }
-    
+
     video.muted = true;
-    video.addEventListener('loadedmetadata', initCanvas, false)
-    video.addEventListener('timeupdate', drawFrame, false)
-    video.src = file
-    video.currentTime = startAt
-    video.play()
-  })
+    video.addEventListener("loadedmetadata", initCanvas, false);
+    video.addEventListener("timeupdate", drawFrame, false);
+    video.src = file;
+    video.currentTime = startAt;
+    video.play();
+  });
 }
 
-
 function App() {
-  const [ video, setVideo ] = useState()
-  const [ extractedImgs, setExtractedImgs ] = useState([])
+  const [video, setVideo] = useState();
+  const [progress, setProgress] = useState(0);
 
   return (
     <div className="app">
       <h1>Client side Video Trimmer (Experimental)</h1>
       <div>
-        <h2>
-          1. Choose video file to upload (support only mp4)
-        </h2>
-        <input type="file" accept="video/mp4" onChange={(e) => setVideo(URL.createObjectURL(e.target.files[0]))} />
+        <h2>1. Choose video file to upload (support only mp4)</h2>
+        <input
+          type="file"
+          accept="video/mp4"
+          onChange={e => setVideo(URL.createObjectURL(e.target.files[0]))}
+        />
       </div>
       <div>
-        <h2>
-          2. Trim Video
-        </h2>
-        {
-          video && (
-            <VideoEditor
-              video={video}
-              onOk={({ startOffset, endOffset }) => {
-                return extractFrames(video, startOffset, endOffset).then(setExtractedImgs)
-              }}
-            />
-          )
-        }
+        <h2>2. Trim Video</h2>
+        {video && <VideoEditor video={video} onOk={onTrimVideo} />}
       </div>
-      <div id="progress"></div>
-      {
-        extractedImgs.map(image => {
-          return <img key={image} src={image} />
-        })
-      }
+      <div id="progress">{progress}%</div>
+      <video id="preview-video" width={600} height={500} />
     </div>
-  )
+  );
 }
 
 export default App;
